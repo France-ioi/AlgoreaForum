f25cf12d816f831c8ae6b89387139ac395ded959
Thomas Juster <juster.thomas@gmail.com>
2022-05-24 12:04:53 +0200

    fix lint for db/peers file

--- src/db/peers.ts
+++ src/db/peers.ts
@@ -10,13 +10,22 @@ export interface Peer {
   connectionId: string,
   status: 'ASSISTANT_FREE' | 'ASSISTANT_BUSY' | 'TRAINEE_WAITING' | 'TRAINEE_BUSY',
 }
-export const isPeer = (data: any): data is Peer => data && typeof data === 'object' && typeof data.connectionId === 'string' &&
-  typeof data.status === 'string' && [ 'ASSISTANT_FREE', 'ASSISTANT_BUSY', 'TRAINEE_WAITING', 'TRAINEE_BUSY' ].includes(data.status);
 
+export const isPeer = (data: any): data is Peer => {
+  if (typeof data !== 'object' || data === null) return false;
+  const { connectionId, status } = data as Record<string, unknown>;
+  if (typeof connectionId !== 'string') return false;
+  if (typeof status !== 'string') return false;
+  if (![ 'ASSISTANT_FREE', 'ASSISTANT_BUSY', 'TRAINEE_WAITING', 'TRAINEE_BUSY' ].includes(status)) return false;
+  return true;
+};
+
+// AWS uses PascalCase for everything, so we need to disable temporarily the casing lint rules
+/* eslint-disable @typescript-eslint/naming-convention */
 class PeersTable {
   constructor(private dynamo: AWS.DynamoDB.DocumentClient) {}
 
-  async add(peer: Peer) {
+  async add(peer: Peer): Promise<void> {
     const seconds = 1000;
     const minutes = 60*seconds;
     const hours = 60*minutes;
@@ -32,7 +41,7 @@ class PeersTable {
     }).promise();
   }
 
-  async update(connectionId: string, status: Peer['status']) {
+  async update(connectionId: string, status: Peer['status']): Promise<void> {
     await this.dynamo.update({
       TableName: peersTableName,
       Key: { connectionId },
@@ -42,7 +51,7 @@ class PeersTable {
     }).promise();
   }
 
-  async delete(connectionId: string) {
+  async delete(connectionId: string): Promise<void> {
     await this.dynamo.delete({
       TableName: peersTableName,
       Key: { connectionId },
@@ -67,5 +76,6 @@ class PeersTable {
     return (result.Item) as Peer;
   }
 }
+/* eslint-enable @typescript-eslint/naming-convention */
 
 export const peersTable = new PeersTable(dynamo);

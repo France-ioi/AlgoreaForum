2cae7864ae59bb4d8e7089accbbde80ed36b68a0
Thomas Juster <juster.thomas@gmail.com>
2022-06-08 14:19:00 +0200

    fix follow handler after rebase

--- src/threads/follow.spec.ts
+++ src/threads/follow.spec.ts
@@ -1,6 +1,6 @@
 import * as parsers from '../parsers';
 import * as messages from './messages';
-import { mockCallback, mockContext, mockEvent } from '../testutils/lambda';
+import { mockContext, mockEvent } from '../testutils/lambda';
 import { tokenData } from '../testutils/mocks';
 import { handler } from './follow';
 import { ForumTable, ThreadEvent } from './table';
@@ -13,58 +13,46 @@ describe('follow', () => {
   const getThreadEventsStub = jest.spyOn(ForumTable.prototype, 'getThreadEvents');
 
   beforeEach(() => {
+    jest.resetAllMocks();
     getConnectionIdStub.mockReturnValue(connectionId);
     addThreadEventStub.mockReturnValue(Promise.resolve({} as any));
     getThreadEventsStub.mockReturnValue(Promise.resolve([]));
   });
 
-  it('should fail (401) when token data is invalid', async () => {
+  it('should fail when token data is invalid', async () => {
     getTokenDataStub.mockImplementationOnce(() => {
       throw new Error('...');
     });
-    await expect(handler(mockEvent(), mockContext(), mockCallback())).resolves.toEqual({
-      statusCode: 401,
-      body: '',
-    });
+    await expect(handler(mockEvent(), mockContext())).rejects.toThrow(Error);
   });
 
-  it('should fail gracefully when adding follow event fails', async () => {
+  it('should fail when adding follow event fails', async () => {
     const data = tokenData(2);
     getTokenDataStub.mockReturnValueOnce(data);
     addThreadEventStub.mockReturnValueOnce(Promise.reject(new Error('...')));
-    await expect(handler(mockEvent(), mockContext(), mockCallback())).resolves.toEqual({
-      statusCode: 401,
-      body: '',
-    });
+    await expect(handler(mockEvent(), mockContext())).rejects.toThrow(Error);
     expect(addThreadEventStub).toHaveBeenCalledTimes(1);
   });
 
-  it('should fail gracefully when listing last events fails', async () => {
+  it('should fail when listing last events fails', async () => {
     const data = tokenData(2);
     getTokenDataStub.mockReturnValueOnce(data);
     getThreadEventsStub.mockReturnValueOnce(Promise.reject(new Error('...')));
-    await expect(handler(mockEvent(), mockContext(), mockCallback())).resolves.toEqual({
-      statusCode: 401,
-      body: '',
-    });
+    await expect(handler(mockEvent(), mockContext())).rejects.toThrow(Error);
     expect(getThreadEventsStub).toHaveBeenCalledTimes(1);
   });
 
-  it('should fail gracefully when parsing the connection id fails', async () => {
+  it('should fail when parsing the connection id fails', async () => {
     const data = tokenData(2);
     getTokenDataStub.mockReturnValueOnce(data);
     getConnectionIdStub.mockImplementationOnce(() => {
       throw new Error('...');
     });
-    await expect(handler(mockEvent(), mockContext(), mockCallback())).resolves.toEqual({
-      statusCode: 401,
-      body: '',
-    });
+    await expect(handler(mockEvent(), mockContext())).rejects.toThrow(Error);
     expect(addThreadEventStub).not.toHaveBeenCalled();
   });
 
   describe('with valid data', () => {
-    let result: any;
     const data = tokenData(1);
     const last20Events: ThreadEvent[] = [];
     const sendStub = jest.spyOn(messages, 'send');
@@ -75,11 +63,7 @@ describe('follow', () => {
       getTokenDataStub.mockReturnValue(data);
       addThreadEventStub.mockReturnValue(Promise.resolve({} as any));
       getConnectionIdStub.mockReturnValue(connectionId);
-      result = await handler(mockEvent(), mockContext(), mockCallback());
-    });
-
-    it('should succeed (201) when token data is valid', () => {
-      expect(result).toEqual({ statusCode: 201, body: '' });
+      await handler(mockEvent(), mockContext());
     });
 
     it('should have added a thread event "follow"', () => {
--- src/threads/follow.ts
+++ src/threads/follow.ts
@@ -1,6 +1,6 @@
-import { APIGatewayProxyHandler } from 'aws-lambda';
 import { dynamodb } from '../dynamodb';
 import { extractTokenData, getConnectionId } from '../parsers';
+import type { SocketHandler } from '../utils/types';
 import { send } from './messages';
 import { ForumTable } from './table';
 
@@ -9,24 +9,18 @@ const seconds = 1;
 const minutes = 60 * seconds;
 const hours = 60 * minutes;
 
-export const handler: APIGatewayProxyHandler = async event => {
-  try {
-    const connectionId = getConnectionId(event);
-    const tokenData = extractTokenData(event);
-    const { participantId, itemId, userId } = tokenData;
+export const handler: SocketHandler = async event => {
+  const connectionId = getConnectionId(event);
+  const tokenData = extractTokenData(event);
+  const { participantId, itemId, userId } = tokenData;
 
-    await forumTable.addThreadEvent(participantId, itemId, {
-      type: 'follow',
-      connectionId,
-      ttl: 12 * hours, // in seconds
-      userId,
-    });
+  await forumTable.addThreadEvent(participantId, itemId, {
+    type: 'follow',
+    connectionId,
+    ttl: 12 * hours, // in seconds
+    userId,
+  });
 
-    const events = await forumTable.getThreadEvents(participantId, itemId, { limit: 20, asc: false });
-    await send(connectionId, events);
-
-    return { statusCode: 201, body: '' };
-  } catch (error) {
-    return { statusCode: 401, body: '' }; // user is unauthenticated
-  }
+  const events = await forumTable.getThreadEvents(participantId, itemId, { limit: 20, asc: false });
+  await send(connectionId, events);
 };

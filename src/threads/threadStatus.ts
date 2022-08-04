f5a947450ef92de18c21b74a10768bd15f621256
Thomas Juster <juster.thomas@gmail.com>
2022-08-04 16:15:06 +0200

    change thread-status to an http endpoint

--- src/threads/threadStatus.ts
+++ src/threads/threadStatus.ts
@@ -1,25 +1,19 @@
 import type { APIGatewayProxyHandler } from 'aws-lambda';
 import { dynamodb } from '../dynamodb';
-import { badRequest, ok, serverError, unauthorized } from '../utils/responses';
-import { send } from './messages';
+import { ok, serverError, unauthorized } from '../utils/responses';
 import { extractTokenData } from '../utils/parsers';
 import { ForumTable } from './table';
 
 const forumTable = new ForumTable(dynamodb);
 
 export const handler: APIGatewayProxyHandler = async event => {
-  if (!event.requestContext.connectionId) return badRequest();
-
   const tokenData = extractTokenData(event);
   if (!tokenData) return unauthorized();
   const { participantId, itemId } = tokenData;
 
   try {
     const status = await forumTable.getThreadStatus(participantId, itemId);
-
-    await send(event.requestContext.connectionId, [{ status }]);
-
-    return ok();
+    return ok(JSON.stringify({ status }));
   } catch {
     return serverError();
   }
